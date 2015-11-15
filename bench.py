39919d77de145bb0281abb761c03e275a0c4a5fe
Andrzej Cichocki <combatopera@users.noreply.github.com>
2015-11-15 17:26:06 +0000

    better

--- bench.py
+++ bench.py
@@ -8,14 +8,18 @@ def sum1(n, x, y, out):
         out[i] = x[i] + y[i]
 
 def main():
+    trials = 10
     n = 100000
     x = np.arange(n, dtype = np.float32)
     y = np.arange(n, dtype = np.float32) * 2
     for s in sum1, sum2:
         out = np.empty(n, dtype = np.float32)
-        t = time.time()
-        s(n, x, y, out)
-        print time.time() - t
+        cum = 0
+        for _ in xrange(trials):
+            t = time.time()
+            s(n, x, y, out)
+            cum += time.time() - t
+        print cum / trials
         print out[:5], out[-5:]
 
 if '__main__' == __name__:
