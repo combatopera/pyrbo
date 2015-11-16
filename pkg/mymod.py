e8078d2f030ced24c1d86f407b585546e8f7b452
Andrzej Cichocki <combatopera@users.noreply.github.com>
2015-11-16 17:26:57 +0000

    improve

--- pkg/mymod.py
+++ pkg/mymod.py
@@ -2,7 +2,7 @@ import numpy as np
 from turbo import turbo, T
 
 @turbo(i = np.uint32, n = np.uint32, x = [np.float32], y = [np.float32], out = [np.float32])
-def sum(n, x, y, out):
+def tsum(n, x, y, out):
     for i in xrange(n):
         out[i] = x[i] + y[i]
 
