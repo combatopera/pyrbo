2123b00f0f17497ef3ddb267126aec03ad40b742
Andrzej Cichocki <combatopera@users.noreply.github.com>
2015-11-16 17:15:53 +0000

    add a generic function

--- pkg/mymod.py
+++ pkg/mymod.py
@@ -1,7 +1,12 @@
 import numpy as np
-from turbo import turbo
+from turbo import turbo, T
 
 @turbo(i = np.uint32, n = np.uint32, x = [np.float32], y = [np.float32], out = [np.float32])
 def sum(n, x, y, out):
+    for i in xrange(n):
+        out[i] = x[i] + y[i]
+
+@turbo([np.float32, np.int64], i = np.uint32, n = np.uint32, x = [T], y = [T], out = [T])
+def gsum(n, x, y, out):
     for i in xrange(n):
         out[i] = x[i] + y[i]
