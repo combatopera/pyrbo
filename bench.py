50cbdefda37a3a8f1eeb560c7c4d5f5f6bc5ef5b
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2015-11-19 08:26:43 +0000

    interestinger results

--- bench.py
+++ bench.py
@@ -12,23 +12,28 @@ def npsum(n, x, y, out):
     out[:n] += y[:n]
 
 def main():
-    n = 500000
+  quarter = 3
+  for n in 100, 1000, 10000, 100000:
+    print n
     x = np.arange(n, dtype = np.float32)
     y = np.arange(n, dtype = np.float32) * 2
-    for task in pysum, npsum, tsum, gsum[np.float32]:
+    for task in npsum, tsum, gsum[np.float32], pysum:
         print task.__name__
         out = np.empty(n, dtype = np.float32)
         times = []
-        for _ in xrange(8):
+        for _ in xrange(quarter * 4):
             t = time.time()
             task(n, x, y, out)
             times.append(time.time() - t)
         times.sort()
-        q1 = (times[1] + times[2]) / 2
-        q2 = (times[3] + times[4]) / 2
-        q3 = (times[5] + times[6]) / 2
-        print ' '.join("%.6f" % t for t in [q1, q2, q3])
-        print out[:5], out[-5:]
+        quartiles = [(times[quarter * q - 1] + times[quarter * q]) / 2 for q in [1, 2, 3]]
+        if npsum == task:
+            scale = quartiles[1]
+            check = out
+            print check[:5], check[-5:]
+        elif not np.array_equal(check, out):
+            raise Exception
+        print ' '.join("%10.6f" % (t/scale) for t in quartiles)
 
 if '__main__' == __name__:
     main()
