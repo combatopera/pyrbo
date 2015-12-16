cc14a0de5ebd587e0a7fe3fa81f3236a325e6ef3
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2015-12-16 07:18:00 +0000

    unit test

---
+++ pkg/test_mymod.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+# Copyright 2014 Andrzej Cichocki
+
+# This file is part of turbo.
+#
+# turbo is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# turbo is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with turbo.  If not, see <http://www.gnu.org/licenses/>.
+
+import unittest
+import numpy as np
+from mymod import tsum, gsum
+
+def pysum(n, x, y, out):
+    for i in xrange(n):
+        out[i] = x[i] + y[i]
+
+def npsum(n, x, y, out):
+    out[:n] = x[:n]
+    out[:n] += y[:n]
+
+class TestTurbo(unittest.TestCase):
+
+    def test_works(self):
+        n = 100000
+        x = np.arange(n, dtype = np.float32)
+        y = np.arange(n, dtype = np.float32) * 2
+        expected = np.empty(n, dtype = np.float32)
+        pysum(n, x, y, expected)
+        for task in npsum, tsum, gsum[np.float32]:
+            actual = np.empty(n, dtype = np.float32)
+            task(n, x, y, actual)
+            self.assertTrue(np.array_equal(expected, actual))
+
+if '__main__' == __name__:
+    unittest.main()
