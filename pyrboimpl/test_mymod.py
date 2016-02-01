f823dfbfe2fe04a52722fce8f4fe489903e8b46a
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2016-02-01 07:15:28 +0000

    inline

--- pyrboimpl/test_mymod.py
+++ pyrboimpl/test_mymod.py
@@ -17,8 +17,11 @@
 # You should have received a copy of the GNU General Public License
 # along with pyrbo.  If not, see <http://www.gnu.org/licenses/>.
 
-import unittest, numpy as np
-from pyrbo import turbo, T
+from __future__ import division
+import unittest, numpy as np, logging, time
+from leaf import turbo, T
+
+log = logging.getLogger(__name__)
 
 def pysum(n, x, y, out):
     for i in xrange(n):
@@ -50,6 +53,37 @@ class TestTurbo(unittest.TestCase):
             actual = np.empty(n, dtype = np.float32)
             task(n, x, y, actual)
             self.assertTrue(np.array_equal(expected, actual))
+
+class TestSpeed(unittest.TestCase):
+
+    ntomaxreldiff = {100: .1} # I guess in small array case we see some setup overhead.
+
+    def test_fastenough(self):
+        quarter = 3
+        nandtasktoquartiles = {}
+        for n in 100, 1000, 10000, 100000:
+            log.info("n: %s", n)
+            x = np.arange(n, dtype = np.float32)
+            y = np.arange(n, dtype = np.float32) * 2
+            for task in npsum, tsum, gsum[T, np.float32]:
+                log.info("task: %s", task)
+                out = np.empty(n, dtype = np.float32)
+                times = []
+                for _ in xrange(quarter * 4):
+                    t = time.time()
+                    task(n, x, y, out)
+                    times.append(time.time() - t)
+                times.sort()
+                quartiles = [(times[quarter * q - 1] + times[quarter * q]) / 2 for q in [1, 2, 3]]
+                log.info("quartiles: %s", ' '.join("%.9f" % q for q in quartiles))
+                nandtasktoquartiles[n, task] = quartiles
+        for (n, task), quartiles in nandtasktoquartiles.iteritems():
+            if npsum != task:
+                maxreldiff = self.ntomaxreldiff.get(n, 0)
+                for qi, tq in enumerate(quartiles):
+                    npq = nandtasktoquartiles[n, npsum][qi]
+                    reldiff = (tq - npq) / tq
+                    self.assertTrue(reldiff <= maxreldiff, "(n = %r, task = %r, quartile = %r, reldiff = %r, maxreldiff = %r)" % (n, task, 1+qi, reldiff, maxreldiff))
 
 if '__main__' == __name__:
     unittest.main()
